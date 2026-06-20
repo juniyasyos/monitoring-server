c3ef77610771dc5352e7940523477daa26966406
juniyasyos <juniyasyos@gmail.com>
2026-06-20 11:51:38 +0700

    update: test with 200VU with k6 service for iam login route

--- k6/scripts/test.js
+++ k6/scripts/test.js
@@ -3,23 +3,30 @@ import { check, sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '30s', target: 50 },   // pemanasan
-    { duration: '1m', target: 100 },   // naik ke 100 user
-    { duration: '1m', target: 200 },   // naik ke 200 user
-    { duration: '2m', target: 400 },   // puncak 400 user
-    { duration: '1m', target: 400 },   // tahan 400 user
-    { duration: '30s', target: 0 },    // turun perlahan
+    { duration: '30s', target: 50 },    // naik ke 50 VU
+    { duration: '1m', target: 50 },     // tahan 50 VU
+
+    { duration: '30s', target: 100 },   // naik ke 100 VU
+    { duration: '1m', target: 100 },    // tahan 100 VU
+
+    { duration: '30s', target: 150 },   // naik ke 150 VU
+    { duration: '1m', target: 150 },    // tahan 150 VU
+
+    { duration: '30s', target: 200 },   // naik ke 200 VU
+    { duration: '2m', target: 200 },    // tahan 200 VU
+
+    { duration: '30s', target: 0 },     // turun perlahan
   ],
 
   thresholds: {
     http_req_failed: ['rate<0.01'],        // error maksimal 1%
-    http_req_duration: ['p(95)<1000'],     // 95% request di bawah 1 detik
+    http_req_duration: ['p(95)<1000'],     // 95% request harus < 1 detik
     checks: ['rate>0.99'],                 // 99% check harus sukses
   },
 };
 
 export default function () {
-  const TARGET_URL = 'http://192.168.1.9:8110';
+  const TARGET_URL = 'http://192.168.1.9:8110/login';
 
   const res = http.get(TARGET_URL);
 
